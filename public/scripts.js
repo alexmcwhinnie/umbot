41df2d289bd3a3051c6f7ee4724cfa972d8c3479
Alex McWhinnie <alexmcwhinnie@Alexanders-MacBook-Pro.local>
2016-10-03 17:55:20 -0500

    RFID integrated

--- public/scripts.js
+++ public/scripts.js
@@ -14,10 +14,13 @@ $(document).ready(function(){
 // 	});
 	var socket = io.connect("http://73.94.56.102");
 
+		socket.on('uid', function (data) {
+	   		console.log(data.uid);
+	  	});
+
 	  $('.checkout').click(function() {
 		console.log('button pressed')
 	    socket.emit('chat message', { my: 'data, yo' });
-
 	  });
 
 
--- tessel-hardware/server.js
+++ tessel-hardware/server.js
@@ -4,12 +4,7 @@ var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-// Socket integration test /////////////////////////////// (please work!)
-io.on('connection', function(socket){
-  blinking();
-});
-//////////////////////////////////////////////////////////
+var rfidlib = require('rfid-pn532');
 
 
 app.use(function(req, res, next) {
@@ -21,6 +16,8 @@ app.use(function(req, res, next) {
 var test = 'ABCDEFGHIJ';
 var pin = tessel.port.A.pin[4];
 var pin2 = tessel.port.A.pin[5];
+var rfid = rfidlib.use(tessel.port['A']); 
+
 
 var checkout = path.join(__dirname, '/public/checkout.mp3');
 var sound1 = new av.Player(checkout);
@@ -33,20 +30,48 @@ var sound3 = new av.Player(intro);
 
 pin.output(1);  // turn pin high (on)
 //sound3.play();  // Play intro
+var checkingOut = false;
+var UID_data = null;
 
-// Socket integration test /////////////////////////////// (please work!)
-var data = 'boop boop, im the server';
+// RFID Integration test /////////////////////////////// 
+rfid.on('ready', function (version) {
+  console.log('Ready to read RFID card');
+
+  rfid.on('data', function(card) {
+    console.log('UID:', card.uid.toString('hex'));
+    
+    // When RFID card is scanned, send UID to client. Client will then put in DB. Prep UID for socket.io
+    UID_data = card.uid.toString('hex');
+  });
+});
+
+rfid.on('error', function (err) {
+  console.error(err);
+});
+//////////////////////////////////////////////////////////
+
+// Socket integration test /////////////////////////////// 
+
 //does 'socket.emit' send, and 'socket.on' receive?
 io.on('connection', function(socket){
 	console.log('socket connected...');
-  socket.on('chat message', function(data){
-    console.log('message from client: ' + data.my);
-    //sound3.play();  // Play intro
-    checkout2();
+
+	// Send UID to client when RFID is swiped. How? <----------------------- THIS LINE IS KEY
+	socket.emit('uid', { uid: UID_data });
+
+ 	socket.on('chat message', function(data){
+	    console.log('message from client: ' + data.my);
+	    checkout2();
+
+		// Send UID to client when RFID is swiped. How?
+		socket.emit('uid', { uid: UID_data });
 
   });
 });
 //////////////////////////////////////////////////////////
+
+
+
 
 
 app.get('/', function (req, res) {
@@ -88,22 +113,33 @@ function checkout() {
 }
 
 function checkout2() {
-	sound1.play();
-	solenoid();  // turn pin high (on)
-	var blink = setInterval(blinking ,500);
-	setTimeout(function( ) { clearInterval( blink ); }, 8000);
-	setTimeout(function(){
-		// Hurry
-		sound2.play();
-		var blink = setInterval(blinking ,350);
-		setTimeout(function( ) { clearInterval( blink ); }, 2700);
-	}, 8000);
-	// Lock
-	setTimeout(function(){
-		solenoid2();
-	}, 10700);
+	if (checkingOut == false) {
+		checkingOut = true;
+		sound1.play();
+		solenoid();  // turn pin high (on)
+		var blink = setInterval(blinking ,500);
+		setTimeout(function( ) { clearInterval( blink ); }, 8000);
+		setTimeout(function(){
+			// Hurry
+			sound2.play();
+			var blink = setInterval(blinking ,350);
+			setTimeout(function( ) { clearInterval( blink ); }, 2700);
+		}, 8000);
+		// Lock
+		setTimeout(function(){
+			solenoid2();
+			checkingOut = false;
+		}, 10700);
+		
+	}
 }
 
+// function endCheckout() {
+// 	setTimeout(function(){
+// 		// Hurry
+// 		setTimeout(function( ) { clearInterval( blink );
+// 	 };
+// }
 
 
 // Checkout
